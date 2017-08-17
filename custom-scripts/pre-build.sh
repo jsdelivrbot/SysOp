c8109d54715b2191db04380985035a71af160069
AnnyFigueira <anny.haruka@gmail.com>
2017-08-17 13:30:55 -0300

    webserver fix

--- custom-scripts/pre-build.sh
+++ custom-scripts/pre-build.sh
@@ -6,4 +6,7 @@
   cp $BASE_DIR/../custom-scripts/web-server.py $BASE_DIR/target/etc/init.d
   chmod +x $BASE_DIR/target/etc/init.d/web-server.py
   
-    cp $BASE_DIR/../custom-scripts/index.html $BASE_DIR/target/
+  cp $BASE_DIR/../custom-scripts/index.html $BASE_DIR/target/etc/inid.d
+
+  cp $BASE_DIR/../custom-scripts/web-server-config $BASE_DIR/target/etc/init.d
+  chmod +x $BASE_DIR/target/etc/init.d/web-server-config
